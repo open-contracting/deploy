85a771bfadab8be4a7272b389ed0c1ec73b0c4dd
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-11-11 17:00:41 -0500

    Use rsync instead of lftp

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -25,21 +25,8 @@ chmod 600 id_rsa
 echo "Add the host key for the remote server..."
 echo 'staging.standard.open-contracting.org ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDk8O226B/sYkPqyHdNBdjUFCEpT9IMdUxgFXEOtlPq1QnwTgHY76PsaOin7KhJcBrm8RAOuzoOIrKfgUJjXoxCtx1edp594tD8OChF5koHyO8YkQVlJmH8LrV16txsxokfh2F31ofRIVMk+TXiEfvR4+WehqeR24TwnXzlLIv1KfMJB7znTDdwqZS3uONKjlNNzSBNNIvCZ4WTI6etVlCzQgv4HL9QllKGfk1ctDuwOgsGPMT8f5NNPhI/z7kZkNbcrHJ5Mo6ZtF26qFmZ3Hy6vxJAQ2C4/x/Zemtb0MbIvI4Qlghh3bl5lER1rB54oMg+DidJ36qMrbqEtZxrBwvP' >> ~/.ssh/known_hosts
 
-echo "Get LFTP binary if necessary..."
-if hash lftp 2>/dev/null; then
-    LFTP=lftp
-else
-    wget "https://raw.githubusercontent.com/open-contracting/deploy/master/lftp"
-    chmod a+x ./lftp
-    LFTP=./lftp
-fi
-
-# Make a test SSH connection, as LFTP doesn't output key errors well.
-# ssh -i id_rsa ocds-docs@staging.standard.open-contracting.org
-
 echo "Copy the built files to the remote server..."
-# See http://lftp.yar.ru/lftp-man.html
-$LFTP -c "set sftp:connect-program \"ssh -i id_rsa\"; connect sftp://ocds-docs:xxx@staging.standard.open-contracting.org; mirror -eRv build web/$PATH_PREFIX$TRAVIS_BRANCH"
+rsync -av --delete-after -e "ssh -i id_rsa" build/ ocds-docs@staging.standard.open-contracting.org:web/$PATH_PREFIX$TRAVIS_BRANCH
 
 echo "Update the search index..."
 curl "https://standard-search.open-contracting.org/v1/index_ocds?secret=${SEARCH_SECRET}&version=$(echo $PATH_PREFIX | sed 's/\//%2F/g')${TRAVIS_BRANCH}&langs=${LANGS}"
