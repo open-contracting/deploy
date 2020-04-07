07a81893d54b5e5072b3e861a0490d1a82075935
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-04-07 00:35:14 -0400

    Update deploy-docs.sh for GitHub Actions instead of Travis

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -26,7 +26,7 @@ echo "Add the host key for the remote server..."
 echo 'staging.standard.open-contracting.org ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDk8O226B/sYkPqyHdNBdjUFCEpT9IMdUxgFXEOtlPq1QnwTgHY76PsaOin7KhJcBrm8RAOuzoOIrKfgUJjXoxCtx1edp594tD8OChF5koHyO8YkQVlJmH8LrV16txsxokfh2F31ofRIVMk+TXiEfvR4+WehqeR24TwnXzlLIv1KfMJB7znTDdwqZS3uONKjlNNzSBNNIvCZ4WTI6etVlCzQgv4HL9QllKGfk1ctDuwOgsGPMT8f5NNPhI/z7kZkNbcrHJ5Mo6ZtF26qFmZ3Hy6vxJAQ2C4/x/Zemtb0MbIvI4Qlghh3bl5lER1rB54oMg+DidJ36qMrbqEtZxrBwvP' >> ~/.ssh/known_hosts
 
 echo "Copy the built files to the remote server..."
-rsync -av --delete-after -e "ssh -i id_rsa" build/ ocds-docs@staging.standard.open-contracting.org:web/$PATH_PREFIX$TRAVIS_BRANCH
+rsync -av --delete-after -e "ssh -i id_rsa" build/ ocds-docs@staging.standard.open-contracting.org:web/$PATH_PREFIX${GITHUB_REF##*/}
 
 echo "Update the search index..."
-curl "https://standard-search.open-contracting.org/v1/index_ocds?secret=${SEARCH_SECRET}&version=$(echo $PATH_PREFIX | sed 's/\//%2F/g')${TRAVIS_BRANCH}&langs=${LANGS}"
+curl "https://standard-search.open-contracting.org/v1/index_ocds?secret=${SEARCH_SECRET}&version=$(echo $PATH_PREFIX | sed 's/\//%2F/g')${GITHUB_REF##*/}&langs=${LANGS}"
