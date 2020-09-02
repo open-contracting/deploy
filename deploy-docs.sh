ce236593f5c5cf6d4d1b04e58cfd3620475061e7
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-09-02 15:54:55 -0400

    If indexing fails, the deploy should fail

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -19,4 +19,4 @@ echo "Copy the built files to the remote server..."
 rsync -avz --delete-after build/ ocds-docs@staging.standard.open-contracting.org:web/$PATH_PREFIX${GITHUB_REF##*/}
 
 echo "Update the search index..."
-curl "https://standard-search.open-contracting.org/v1/index_ocds?secret=${SEARCH_SECRET}&version=$(echo $PATH_PREFIX | sed 's/\//%2F/g')${GITHUB_REF##*/}&langs=${LANGS}"
+curl --fail "https://standard-search.open-contracting.org/v1/index_ocds?secret=${SEARCH_SECRET}&version=$(echo $PATH_PREFIX | sed 's/\//%2F/g')${GITHUB_REF##*/}&langs=${LANGS}"
