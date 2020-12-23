b287a2e40afb733a1813162ac20c2288e44a76ae
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-12-23 12:01:50 -0500

    docs: Update deploy-docs.sh to use ocdsindex

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -1,26 +1,9 @@
-set -e
+#!/usr/bin/env bash
 
-if [ -z ${PATH_PREFIX+x} ]; then
-    echo "\$PATH_PREFIX is not set, exiting."
-    exit
-fi
+set -xeuo pipefail
 
-if [ -z "$SEARCH_SECRET" ]; then
-    echo "\$SEARCH_SECRET is not set or empty, exiting."
-    exit
-fi
-
-if [ -z "$LANGS" ]; then
-    echo "\$LANGS is not set or empty, exiting."
-    exit
-fi
-
-echo "Port knocking"
-# This returns error code 28 (timeout), which is fine. 
 curl --silent --connect-timeout 1 standard.open-contracting.org:8255 || true
+rsync -avz --delete-after build/ ocds-docs@standard.open-contracting.org:web/staging/"$PATH_PREFIX""${GITHUB_REF##*/}"
 
-echo "Copy the built files to the remote server..."
-rsync -avz --delete-after build/ ocds-docs@standard.open-contracting.org:web/staging/$PATH_PREFIX${GITHUB_REF##*/}
-
-echo "Update the search index..."
-curl -sS --fail "https://standard-search.open-contracting.org/v1/index_ocds?secret=${SEARCH_SECRET}&version=$(echo staging/$PATH_PREFIX | sed 's/\//%2F/g')${GITHUB_REF##*/}&langs=${LANGS}"
+ocdsindex sphinx build/ https://standard.open-contracting.org/staging/"$PATH_PREFIX""${GITHUB_REF##*/}"/ > documents.json
+ocdsindex index https://standard.open-contracting.org:9200 documents.json
