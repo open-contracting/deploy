1ea8f1c841050e19076e75c1b83c390acbb6f3c9
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-12-26 17:15:31 -0500

    elasticsearch: Fix path to ReadOnlyREST plugin

--- salt/elasticsearch/files/readonlyrest-installer.sh
+++ salt/elasticsearch/files/readonlyrest-installer.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/expect -f
 
 # Based on output of:
-# autoexpect /usr/share/elasticsearch/bin/elasticsearch-plugin install --silent file:///root/readonlyrest-{{ version }}.zip
+# autoexpect /usr/share/elasticsearch/bin/elasticsearch-plugin install --silent file:///opt/readonlyrest-{{ version }}.zip
 
 set timeout -1
-spawn /usr/share/elasticsearch/bin/elasticsearch-plugin install --silent file:///root/readonlyrest-{{ version }}.zip
+spawn /usr/share/elasticsearch/bin/elasticsearch-plugin install --silent file:///opt/readonlyrest-{{ version }}.zip
 expect -exact "Continue with installation? \[y/N\]"
 send -- "y\r"
 expect eof
