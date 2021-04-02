9b6ee8e74b145944e6c10e59316431be6c5672b7
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-04-01 22:45:48 -0400

    deploy-docs: Making profile directory caused script to end early

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -3,7 +3,6 @@
 set -xeuo pipefail
 
 curl --silent --connect-timeout 1 standard.open-contracting.org:8255 || true
-ssh ocds-docs@standard.open-contracting.org mkdir -p /home/ocds-docs/web/staging/"$PATH_PREFIX"
 rsync -az --delete-after build/ ocds-docs@standard.open-contracting.org:web/staging/"$PATH_PREFIX""${GITHUB_REF##*/}"
 
 ocdsindex sphinx build/ https://standard.open-contracting.org/staging/"$PATH_PREFIX""${GITHUB_REF##*/}"/ > documents.json
