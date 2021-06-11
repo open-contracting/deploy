e59035c83279c27306481e14442588f9c159ac68
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-06-11 14:59:40 -0400

    deploy-docs: Update Elasticsearch before updating symlink

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -15,10 +15,10 @@ fi
 curl --silent --connect-timeout 1 standard.open-contracting.org:8255 || true
 rsync -az --delete-after build/ ocds-docs@standard.open-contracting.org:web/"$PREFIX""$PATH_PREFIX""${GITHUB_REF##*/}""$SUFFIX"
 
+ocdsindex sphinx build/ https://standard.open-contracting.org/"$INFIX""$PATH_PREFIX""${GITHUB_REF##*/}"/ > documents.json
+ocdsindex index https://standard.open-contracting.org:9200 documents.json
+
 if [ "$PRODUCTION" == "true" ]; then
     curl --silent --connect-timeout 1 standard.open-contracting.org:8255 || true
     ssh ocds-docs@standard.open-contracting.org "ln -nfs ${GITHUB_REF##*/}-${TIMESTAMP} /home/ocds-docs/web/$PATH_PREFIX${GITHUB_REF##*/}"
 fi
-
-ocdsindex sphinx build/ https://standard.open-contracting.org/"$INFIX""$PATH_PREFIX""${GITHUB_REF##*/}"/ > documents.json
-ocdsindex index https://standard.open-contracting.org:9200 documents.json
