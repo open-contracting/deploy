c87b719ffb017e2543ae1c6896177b8a82fe8561
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-06-11 14:30:24 -0400

    deploy-docs: Add option to deploy directly to production

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -2,8 +2,23 @@
 
 set -xeuo pipefail
 
-curl --silent --connect-timeout 1 standard.open-contracting.org:8255 || true
-rsync -az --delete-after build/ ocds-docs@standard.open-contracting.org:web/staging/"$PATH_PREFIX""${GITHUB_REF##*/}"
+TIMESTAMP=$(date +%s)
 
-ocdsindex sphinx build/ https://standard.open-contracting.org/staging/"$PATH_PREFIX""${GITHUB_REF##*/}"/ > documents.json
+if [ "$ENVIRONMENT" == "production" ]; then
+    PREFIX=""
+    SUFFIX=-"${TIMESTAMP}"
+else
+    PREFIX="staging/"
+    SUFFIX=""
+fi
+
+curl --silent --connect-timeout 1 standard.open-contracting.org:8255 || true
+rsync -az --delete-after build/ ocds-docs@standard.open-contracting.org:web/"$PREFIX""$PATH_PREFIX""${GITHUB_REF##*/}""$SUFFIX"
+
+if [ "$ENVIRONMENT" == "production" ]; then
+    curl --silent --connect-timeout 1 standard.open-contracting.org:8255 || true
+    ssh ocds-docs@standard.open-contracting.org "ln -nfs ${GITHUB_REF##*/}-${TIMESTAMP} /home/ocds-docs/web/$PATH_PREFIX${GITHUB_REF##*/}"
+fi
+
+ocdsindex sphinx build/ https://standard.open-contracting.org/"$INFIX""$PATH_PREFIX""${GITHUB_REF##*/}"/ > documents.json
 ocdsindex index https://standard.open-contracting.org:9200 documents.json
