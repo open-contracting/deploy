036a8874fb596657a657656f3596d0bec44c9501
Rob Hooper <rob@dogsbody.com>
2022-10-11 11:43:53 +0100

    Fixing script formatting

--- salt/redmine/files/redmine_cron.sh
+++ salt/redmine/files/redmine_cron.sh
@@ -3,10 +3,10 @@
 # This file is managed by Dogsbody Technology Ltd.
 #   https://www.dogsbody.com/
 #
-# Description:  A script to pull the latest emails from gmail into redmine 
+# Description:  A script to pull the latest emails from gmail into redmine
 #
 # Usage:  /etc/cron.d/redmine
-#         */5 * * * * root /bin/bash /home/sysadmin-tools/bin/redmine_cron.sh 
+#         */5 * * * * root /bin/bash /home/sysadmin-tools/bin/redmine_cron.sh
 #
 
 API_KEY="{{ pillar.redmine.api_key }}"
@@ -17,13 +17,13 @@ COUNTERFILE=/tmp/readmine_cron_error.txt
 
 # Alert if the cron fails 5 times
 if [[ ${COLLECTMAIL} != '{"count":0,"errors":[]}' ]]; then
-  COUNTER=$(cat ${COUNTERFILE})
-  # Increment the counter. 
-  echo $((COUNTER++)) > ${COUNTERFILE}
-  # If the script has failed more than 5 times then echo output. This will be sent to root.
-  if (( COUNTER >= 5 )); then
-    echo -e "redmine_cron_error\nThe redmine email update cron has failed 5 times successively"
-  fi
+    COUNTER=$(cat ${COUNTERFILE})
+    # Increment the counter.
+    echo $((COUNTER++)) > ${COUNTERFILE}
+    # If the script has failed more than 5 times then echo output. This will be sent to root.
+    if ((COUNTER >= 5)); then
+        echo -e "redmine_cron_error\nThe redmine email update cron has failed 5 times successively"
+    fi
 else
-  echo "0" > ${COUNTERFILE}
+    echo "0" > ${COUNTERFILE}
 fi
