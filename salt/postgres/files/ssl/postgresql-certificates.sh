dcde22c3a821e2eb259efcaaf106a15d70f738eb
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-06-27 22:53:41 -0400

    postgres(chore): Disable shellcheck for Jinja syntax

--- salt/postgres/files/ssl/postgresql-certificates.sh
+++ salt/postgres/files/ssl/postgresql-certificates.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC1083
 cp /etc/apache2/md/domains/"$1"/pubcert.pem /etc/apache2/md/domains/"$1"/privkey.pem /etc/postgresql/{{ pillar.postgres.version }}/main/
+# shellcheck disable=SC1083
 chown postgres:postgres /etc/postgresql/{{ pillar.postgres.version }}/main/pubcert.pem /etc/postgresql/{{ pillar.postgres.version }}/main/privkey.pem
 systemctl restart postgresql
