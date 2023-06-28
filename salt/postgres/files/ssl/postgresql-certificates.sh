2a3bad26776a8bea705953f60d4bf62393cdcc26
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-06-27 23:31:08 -0400

    postgres: Reload (instead of restart) for new certificates https://www.postgresql.org/docs/current/ssl-tcp.html

--- salt/postgres/files/ssl/postgresql-certificates.sh
+++ salt/postgres/files/ssl/postgresql-certificates.sh
@@ -8,4 +8,4 @@ fi
 cp /etc/apache2/md/"$DIRECTORY"/"$1"/pubcert.pem /etc/apache2/md/"$DIRECTORY"/"$1"/privkey.pem /etc/postgresql/{{ pillar.postgres.version }}/main/
 # shellcheck disable=SC1083
 chown postgres:postgres /etc/postgresql/{{ pillar.postgres.version }}/main/pubcert.pem /etc/postgresql/{{ pillar.postgres.version }}/main/privkey.pem
-systemctl restart postgresql
+systemctl reload postgresql.service
