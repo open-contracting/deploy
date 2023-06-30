842daad42ec9f8d2f3470064023a940e0dc4ab12
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-06-30 13:15:18 -0400

    chore(lint): shellcheck

--- salt/kingfisher/files/kingfisher-process.sh
+++ salt/kingfisher/files/kingfisher-process.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 (
-    cd {{ directory }}
+    # shellcheck disable=SC1083
+    cd {{ directory }} || exit
+    # shellcheck disable=SC1083
     docker compose run --rm web python manage.py {{ command }} "$@"
 )
