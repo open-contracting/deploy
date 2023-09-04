d94a6801cc08a6b46a1c9adb6915434376636674
Rob Hooper <rob@dogsbody.com>
2023-09-04 15:58:45 +0100

    feat: save cleared firewall rules

--- salt/core/firewall/files/firewall_reset.sh
+++ salt/core/firewall/files/firewall_reset.sh
@@ -12,6 +12,28 @@ set -euo pipefail
 # shellcheck disable=SC1091
 source /home/sysadmin-tools/firewall-settings.local
 
+echo "Check user is root"
+if [ "$LOGNAME" != "root" ]; then
+    echo "User is not root!"
+    exit 4
+fi
+
+echo "Get iptables location"
+case "${ID}_${VERSION_ID}" in
+ubuntu_22.04 | ubuntu_20.04 | ubuntu_18.04 | debian_10 | debian_9 | debian_8)
+    IPTABLESSAVLOC=/etc/iptables/rules.v4
+    IP6TABLESSAVLOC=/etc/iptables/rules.v6
+    ;;
+centos_7 | redhat-derivative_)
+    IPTABLESSAVLOC=/etc/sysconfig/iptables
+    IP6TABLESSAVLOC=/etc/sysconfig/ip6tables
+    ;;
+*)
+    echo "Failed to determine iptables location! Please update this script appropriately."
+    exit 7
+    ;;
+esac
+
 $IPTABLES -P INPUT ACCEPT
 $IPTABLES -P FORWARD ACCEPT
 $IPTABLES -P OUTPUT ACCEPT
@@ -36,3 +58,7 @@ $IP6TABLES -t mangle -F
 $IP6TABLES -t mangle -X
 $IP6TABLES -t raw -F
 $IP6TABLES -t raw -X
+
+echo "Save iptables to $IPTABLESSAVLOC and $IP6TABLESSAVLOC"
+iptables-save > $IPTABLESSAVLOC
+ip6tables-save > $IP6TABLESSAVLOC
