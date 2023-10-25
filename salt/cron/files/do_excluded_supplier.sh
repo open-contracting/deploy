35514a47b380eb17e74f199cb421cf915bb8185d
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-10-25 18:09:56 -0400

    chore: shfmt

--- salt/cron/files/do_excluded_supplier.sh
+++ salt/cron/files/do_excluded_supplier.sh
@@ -3,13 +3,13 @@
 set -euo pipefail
 
 # https://datosabiertos.dgcp.gob.do/opendata/tablas
-curl -sS https://api.dgcp.gob.do/opendata/proveedores/proveedores_inhabilitados.csv | \
-  # The first column is the RPE.
-  grep -Eo '^[0-9]+,' | \
-  # Sort numerically and uniquely.
-  sort -nu | \
-  # Prefix the identifier scheme.
-  sed -E 's/^(.+),$/DO-RPE-\1/' | \
-  # Replace the table in a transaction.
-  psql postgresql://kingfisher_collect@localhost/kingfisher_collect -c \
-  "BEGIN; DELETE FROM excluded_supplier; COPY excluded_supplier (identifier) FROM stdin DELIMITER ',' CSV HEADER; END;"
+curl -sS https://api.dgcp.gob.do/opendata/proveedores/proveedores_inhabilitados.csv |
+    # The first column is the RPE.
+    grep -Eo '^[0-9]+,' |
+    # Sort numerically and uniquely.
+    sort -nu |
+    # Prefix the identifier scheme.
+    sed -E 's/^(.+),$/DO-RPE-\1/' |
+    # Replace the table in a transaction.
+    psql postgresql://kingfisher_collect@localhost/kingfisher_collect -c \
+        "BEGIN; DELETE FROM excluded_supplier; COPY excluded_supplier (identifier) FROM stdin DELIMITER ',' CSV HEADER; END;"
