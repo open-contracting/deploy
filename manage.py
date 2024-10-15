ce9edace59fdad7f6b8161c6ac5818e68905b986
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-10-15 17:52:48 -0400

    chore: Run ruff format

--- manage.py
+++ manage.py
@@ -14,18 +14,18 @@ import salt.cli.ssh
 import salt.client.ssh
 
 PROVIDERS = (
-    'linode',
-    'hetzner',
+    "linode",
+    "hetzner",
 )
 
 
 def get_provider(target):
-    if 'kingfisher' in target or 'registry' in target:
-        return 'hetzner'
-    return 'linode'
+    if "kingfisher" in target or "registry" in target:
+        return "hetzner"
+    return "linode"
 
 
-def compare(content, get_item, mode='diff', margin=0, expected_providers=None):
+def compare(content, get_item, mode="diff", margin=0, expected_providers=None):
     provider_count = defaultdict(int)
     target_items = defaultdict(list)
     item_providers = defaultdict(lambda: defaultdict(int))
@@ -33,10 +33,10 @@ def compare(content, get_item, mode='diff', margin=0, expected_providers=None):
     target = None
     provider = None
     for line in content.splitlines():
-        if line.startswith('        '):
+        if line.startswith("        "):
             continue
 
-        if line.startswith(' '):
+        if line.startswith(" "):
             item = get_item(line.strip())
             target_items[target].append(item)
             item_providers[item][provider] += 1
@@ -55,23 +55,23 @@ def compare(content, get_item, mode='diff', margin=0, expected_providers=None):
         provider = get_provider(target)
         if provider not in expected_providers:
             continue
-        print(f'{target}:')
+        print(f"{target}:")
         for item in target_items[target]:
             included = item in provider_items[provider]
-            if mode == 'diff' and not included or mode == 'comm' and included:
-                print(f'  {item}')
+            if mode == "diff" and not included or mode == "comm" and included:
+                print(f"  {item}")
 
 
 def salt_ssh(*args):
     # See run.py
-    sys.argv = ['salt-ssh', *args]
+    sys.argv = ["salt-ssh", *args]
     client = salt.cli.ssh.SaltSSH()
     client.parse_args()
     ssh = salt.client.ssh.SSH(client.config)
 
     for target in ssh.targets.values():
         with contextlib.suppress(OSError):
-            socket.create_connection((target['host'], 8255), 1)
+            socket.create_connection((target["host"], 8255), 1)
 
     return subprocess.run(sys.argv, check=True, stdout=subprocess.PIPE).stdout.decode()  # noqa: S603 # trusted input
 
@@ -82,33 +82,37 @@ def cli():
 
 
 @cli.command()
-@click.option('--provider', type=click.Choice(PROVIDERS), default=PROVIDERS, multiple=True,
-              help='the providers to report on')
+@click.option(
+    "--provider", type=click.Choice(PROVIDERS), default=PROVIDERS, multiple=True, help="the providers to report on"
+)
 def services(provider):
     """List services that are not common to all servers of the same provider."""
-    content = salt_ssh('*', 'service.get_all')
+    content = salt_ssh("*", "service.get_all")
     compare(content, lambda line: line.strip()[2:], expected_providers=provider)
 
 
 @cli.command()
-@click.option('--provider', type=click.Choice(PROVIDERS), default=PROVIDERS, multiple=True,
-              help='the providers to report on')
+@click.option(
+    "--provider", type=click.Choice(PROVIDERS), default=PROVIDERS, multiple=True, help="the providers to report on"
+)
 def packages(provider):
     """List packages that are not common to all servers of the same provider."""
-    content = salt_ssh('*', 'pkg.list_pkgs')
+    content = salt_ssh("*", "pkg.list_pkgs")
     compare(content, lambda line: line.strip()[:-1], expected_providers=provider)
 
 
 @cli.command()
-@click.option('--margin', type=int, default=0,
-              help='the margin within which packages are considered to be common to all servers')
-@click.option('--provider', type=click.Choice(PROVIDERS), default=PROVIDERS, multiple=True,
-              help='the providers to report on')
+@click.option(
+    "--margin", type=int, default=0, help="the margin within which packages are considered to be common to all servers"
+)
+@click.option(
+    "--provider", type=click.Choice(PROVIDERS), default=PROVIDERS, multiple=True, help="the providers to report on"
+)
 def autoremove(margin, provider):
     """List packages that can be auto-removed and that are common to all servers of the same provider."""
-    content = salt_ssh('*', 'pkg.autoremove', 'list_only=True')
-    compare(content, lambda line: line.strip()[2:], mode='comm', margin=margin, expected_providers=provider)
+    content = salt_ssh("*", "pkg.autoremove", "list_only=True")
+    compare(content, lambda line: line.strip()[2:], mode="comm", margin=margin, expected_providers=provider)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
