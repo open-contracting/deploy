6d34220567169004c262067389136f15479c5799
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2025-05-05 15:20:08 -0300

    fix(kingfisher): add standard.open-contracting.org to no_proxy
    So that the compile option from the incremental updates won't fail.

--- salt/kingfisher/collect/files/cron.sh
+++ salt/kingfisher/collect/files/cron.sh
@@ -5,7 +5,7 @@ set -eu
 
 cd {{ directory }}
 
-env http_proxy={{ pillar.tinyproxy.url }} https_proxy={{ pillar.tinyproxy.url }} no_proxy=localhost,sentry.io .ve/bin/scrapy crawl \
+env http_proxy={{ pillar.tinyproxy.url }} https_proxy={{ pillar.tinyproxy.url }} no_proxy=localhost,sentry.io,standard.open-contracting.org .ve/bin/scrapy crawl \
     {{ crawl.spider }}{% if 'spider_arguments' in crawl %} {{ crawl.spider_arguments }}{% endif %} \
     -a crawl_time={{ crawl.start_date }}T00:00:00 \
     -s FILES_STORE={{ userdir }}/data \
