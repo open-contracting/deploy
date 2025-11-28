d0e89e1ea8bee368fd698ae654e999438ec6936f
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-11-28 12:27:07 -0500

    chore: Avoid single-use variables

--- salt/prometheus/files/system_metrics.sh
+++ salt/prometheus/files/system_metrics.sh
@@ -1,21 +1,18 @@
 #!/bin/sh
+#
 # Extendable script for custom system metrics
 # https://prometheus.io/docs/instrumenting/exposition_formats/
 
 set -eu
 
-## Mail Queue size
-MAILQUEUECOUNT=$(postqueue -j | wc -l)
-echo "mail_queue_size $MAILQUEUECOUNT"
+echo "mail_queue_size $(postqueue -j | wc -l)"
 
-## mbox size
 (
     cd /var/spool/mail/
     for user in *; do
         if [ -f "$user" ]; then
-            # If the mailbox is zero, grep returns a non-zero exit code.
-            MBOXCOUNT=$(grep -c "^From " "$user" || true)
-            echo "mail_box_size{user=\"$user\"} $MBOXCOUNT"
+            # If the mailbox is empty, grep returns "0" with a non-zero exit code.
+            echo "mail_box_size{user=\"$user\"} $(grep -c "^From " "$user" || true)"
         fi
     done
 )
