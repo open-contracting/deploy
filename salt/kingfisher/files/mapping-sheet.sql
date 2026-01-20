6e933fba2292666e480249f64a3f599d160cf0c9
James McKinney <26463+jpmckinney@users.noreply.github.com>
2026-01-20 11:16:18 -0500

    chore: Fix sqlfluff lints

--- salt/kingfisher/files/mapping-sheet.sql
+++ salt/kingfisher/files/mapping-sheet.sql
@@ -4,30 +4,30 @@ DROP TABLE IF EXISTS reference.mapping_sheets;
 
 CREATE TABLE reference.mapping_sheets (
     id serial PRIMARY KEY,
-    version text,
-    extension text,
+    "version" text,
+    "extension" text,
     section text,
-    path text,
+    "path" text,
     title text,
     description text,
-    type text,
-    range text,
-    values text,
+    "type" text,
+    "range" text,
+    "values" text,
     links text,
     deprecated text,
     "deprecationNotes" text
 );
 
 COPY reference.mapping_sheets (
-    version,
-    extension,
+    "version",
+    "extension",
     section,
-    path,
+    "path",
     title,
     description,
-    type,
-    range,
-    values,
+    "type",
+    "range",
+    "values",
     links,
     deprecated,
     "deprecationNotes"
