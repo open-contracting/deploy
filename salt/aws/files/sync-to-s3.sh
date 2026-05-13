d983d08dec9b864f6293e2ef49f0793148bca9a7
RobHooper <robh@linux.com>
2026-05-13 15:12:06 +0100

    feat: pass args directly to aws cli

--- salt/aws/files/sync-to-s3.sh
+++ salt/aws/files/sync-to-s3.sh
@@ -29,36 +29,17 @@ if [ -z "$S3_SYNC_BUCKET" ]; then
     exit 4
 fi
 
-AWS_ARGS=(--only-show-errors --delete)
-
-# Parse script arguments
-while [[ $# -gt 0 ]]; do
-    case $1 in
-    --exclude)
-        if [ -z "$2" ]; then
-            echo "Missing path."
-            exit 1
-        fi
-        AWS_ARGS+=(--exclude "\"$2\"")
-        shift 2
-        ;;
-    -*)
-        echo "Unknown option"
-        exit 1
-        ;;
-    *)
-        DIRECTORY=$1
-        SAFENAME=${DIRECTORY/#\//}
-        SAFENAME="${SAFENAME/%\//}"
-        shift
-        ;;
-    esac
-done
-
-if [ -e "$DIRECTORY" ]; then
+if [ ! -e "$1" ]; then
     echo "Error: DIRECTORY isn't set or doesn't exist."
     exit 5
 fi
+
+DIRECTORY=$1
+SAFENAME=${DIRECTORY/#\//}
+SAFENAME="${SAFENAME/%\//}"
+shift
+
+AWS_ARGS=(--only-show-errors --delete "${@}")
 
 set +e
 $AWS_CLI s3 sync "${AWS_ARGS[@]}" "$DIRECTORY" "s3://$S3_SYNC_BUCKET/$SAFENAME/"
