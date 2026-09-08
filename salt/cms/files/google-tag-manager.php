794ddb8aadf0d11c345ef96f088d8fb73f9c7488
James McKinney <26463+jpmckinney@users.noreply.github.com>
2026-09-08 10:58:40 -0400

    feat(corporate): Add GTM noscript fallback

--- salt/cms/files/google-tag-manager.php
+++ salt/cms/files/google-tag-manager.php
@@ -9,20 +9,47 @@
  * @package OpenContracting
  */
 
+/**
+ * Return the container ID, or null if this request gets no tag.
+ */
+function opencontracting_gtm_container_id() {
+	if ( is_user_logged_in() ) {
+		return null;
+	}
+
+	return defined( 'GTM_CONTAINER_ID' ) ? GTM_CONTAINER_ID : '{{ GTM_CONTAINER_ID }}';
+}
+
 add_action(
 	'wp_head',
 	function () {
-		if ( is_user_logged_in() ) {
+		$container_id = opencontracting_gtm_container_id();
+
+		if ( null === $container_id ) {
 			return;
 		}
 
-		$container_id = defined( 'GTM_CONTAINER_ID' ) ? GTM_CONTAINER_ID : '{{ GTM_CONTAINER_ID }}';
-
-		// The theme doesn't call wp_body_open(), so there's no place for a <noscript> fallback.
 		// phpcs:ignore WordPress.WP.EnqueuedResources.NonEnqueuedScript -- Tag Manager must load before other scripts.
 		echo "<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});"
 			. "var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;"
 			. "j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})"
 			. "(window,document,'script','dataLayer'," . wp_json_encode( $container_id ) . ');</script>' . "\n";
+	}
+);
+
+// Tag Manager wants this right after <body>, but the theme doesn't call wp_body_open().
+add_action(
+	'wp_footer',
+	function () {
+		$container_id = opencontracting_gtm_container_id();
+
+		if ( null === $container_id ) {
+			return;
+		}
+
+		printf(
+			'<noscript><iframe src="%s" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>' . "\n",
+			esc_url( 'https://www.googletagmanager.com/ns.html?id=' . rawurlencode( $container_id ) )
+		);
 	}
 );
